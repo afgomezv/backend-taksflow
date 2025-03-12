a4817949bf5a86fc488992c0c77d62f75ee7d2bf
afgomezv <afgomezvonline@gmail.com>
2025-03-12 13:47:24 -0500

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config.js";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/ProjectRoutes";
+import { corsConfig } from "./config/cors";
 
 // Connect to MongoDB database
 connectDB();
 
 const server = express();
+
+server.use(cors(corsConfig));
 
 server.use(express.json());
 
