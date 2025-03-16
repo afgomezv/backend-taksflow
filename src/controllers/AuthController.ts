caa694550e545fd0a2da0c641c61e8b20067ac93
afgomezv <afgomezvonline@gmail.com>
2025-03-16 06:01:49 -0500

    hash password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,17 +1,34 @@
 import type { Request, Response } from "express";
+
 import User from "../model/User";
+import { hashPassoword } from "../utils/auth";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password, email } = req.body;
+
+      // check if user already exists
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        const error = new Error(`El usuario ya se encuentra registrado.`);
+        res.status(409).json({ error: error.message });
+        return;
+      }
+
+      // Create a new account
       const user = new User(req.body);
+
+      //hash password
+      user.password = await hashPassoword(password);
       await user.save();
-      res
-        .status(201)
-        .json({
-          message:
-            "Cuenta creada correctamente revisa tu correo electrónico para confirmarla",
-        });
-    } catch (error) {}
+      res.status(201).json({
+        message:
+          "Cuenta creada con éxito. Revisa tu correo electrónico para confirmarla.",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error al crear la cuenta" });
+    }
   };
 }
